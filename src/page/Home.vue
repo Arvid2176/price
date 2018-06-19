d59c233c0fe9377d46034be5eab340cb99e93e97
jianganwei <2550889007@qq.com>
2018-06-19 09:39:00 +0800

    dev

--- src/page/Home.vue
+++ src/page/Home.vue
@@ -1,281 +1,109 @@
+<style scoped>
+  .layout{
+    border: 1px solid #d7dde4;
+    background: #f5f7f9;
+    position: relative;
+    border-radius: 4px;
+    overflow: hidden;
+  }
+  .layout-header-bar{
+    background: #fff;
+    box-shadow: 0 1px 1px rgba(0,0,0,.1);
+  }
+  .layout-logo-left{
+    width: 90%;
+    height: 30px;
+    background: #5b6270;
+    border-radius: 3px;
+    margin: 15px auto;
+  }
+  .menu-icon{
+    transition: all .3s;
+  }
+  .rotate-icon{
+    transform: rotate(-90deg);
+  }
+  .menu-item span{
+    display: inline-block;
+    overflow: hidden;
+    width: 69px;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    vertical-align: bottom;
+    transition: width .2s ease .2s;
+  }
+  .menu-item i{
+    transform: translateX(0px);
+    transition: font-size .2s ease, transform .2s ease;
+    vertical-align: middle;
+    font-size: 16px;
+  }
+  .collapsed-menu span{
+    width: 0px;
+    transition: width .2s ease;
+  }
+  .collapsed-menu i{
+    transform: translateX(5px);
+    transition: font-size .2s ease .2s, transform .2s ease .2s;
+    vertical-align: middle;
+    font-size: 22px;
+  }
+</style>
 <template>
   <div class="layout">
-    <Row type="flex">
-      <i-col :span="spanLeft" class="layout-menu-left">
-        <Menu theme="dark" width="auto" :active-name="this.$route.path" :open-names="openNames" @on-select="menuSelect" accordion>
-          <div class="layout-logo-left">
-            <Icon type="paper-airplane" :size="logoSize"></Icon>
-            <span class="layout-text" style="font-size:25px">BEMOAN</span>
-          </div>
-          <template v-for="(item,index) in $router.options.routes" v-if="!item.hidden">
-            <Submenu :name="item.name" v-if="!item.leaf">
-              <template slot="title">
-                <Icon :type="item.iconCls" :size="iconSize"></Icon>
-                <span class="layout-text">{{item.name}}</span>
-              </template>
-              <template v-for="(child,childIndex) in item.children" v-if="!child.hidden">
-                <Menu-item :name="child.path">{{child.name}}</Menu-item>
-              </template>
-            </Submenu>
-            <template v-if="item.leaf&&item.children.length>0">
-              <Menu-item :name="item.children[0].path">
-                <Icon :type="item.iconCls" :size="iconSize"></Icon>
-                <span class="layout-text">{{item.children[0].name}}</span>
-              </Menu-item>
-            </template>
-          </template>
+    <Layout>
+      <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed">
+        <Menu active-name="1-2" theme="dark" width="auto" :class="menuitemClasses">
+          <MenuItem name="1-1">
+            <Icon type="ios-navigate"></Icon>
+            <span>Option 1</span>
+          </MenuItem>
+          <MenuItem name="1-2">
+            <Icon type="search"></Icon>
+            <span>Option 2</span>
+          </MenuItem>
+          <MenuItem name="1-3">
+            <Icon type="settings"></Icon>
+            <span>Option 3</span>
+          </MenuItem>
         </Menu>
-      </i-col>
-      <i-col :span="spanRight">
-        <div class="layout-header">
-          <!--<i-button type="text" @click="toggleClick">
-                        <Icon type="navicon" size="32"></Icon>
-                    </i-button>-->
-          <div class="userinfo">
-            <Dropdown placement="bottom-end">
-              <span class="head-img">
-               企业信用监测系统
-                <!-- <img src="../assets/user.jpg"> -->
-              </span>
-            </Dropdown>
-          </div>
-        </div>
-        <div class="layout-content">
-          <div class="layout-content-main">
-
-            <router-view></router-view>
-          </div>
-        </div>
-      </i-col>
-    </Row>
-
-    <!-- <Modal v-model="modal1" title="修改密码" @on-ok.prevent="comfirmModifyPS"  @on-cancel="cancel" >
-            <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="100">
-                <Form-item label="原密码" prop="oldPassword">
-                    <Input v-model="formValidate.oldPassword" placeholder="请输入原始密码"></Input>
-                </Form-item>
-                <Form-item label="新密码" prop="newPassword">
-                    <Input v-model="formValidate.newPassword" placeholder="请输入新密码"></Input>
-                </Form-item>
-                 <Form-item label="确认新密码" prop="resetPassword">
-                    <Input v-model="formValidate.resetPassword" placeholder="请再次输入新密码"></Input>
-                </Form-item>
-            </Form>
-        </Modal> -->
+      </Sider>
+      <Layout>
+        <Header :style="{padding: 0}" class="layout-header-bar">
+          <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '20px 20px 0'}" type="navicon-round" size="24"></Icon>
+        </Header>
+        <Content :style="{margin: '20px', background: '#fff', minHeight: '260px'}">
+          Content
+        </Content>
+      </Layout>
+    </Layout>
   </div>
-  <!-- 修改密码 模态框 -->
-
-  <!-- 修改密码 模态框 -->
 </template>
-
 <script>
   export default {
-    data() {
+    data () {
       return {
-        openNames: [this.$route.matched[0].name],
-        curUserName: sessionStorage.getItem('user').replace(/\"/g, ""),
-        modeType: "vertical",
-        spanLeft: 3,
-        spanRight: 21,
-        logoIsDisplay: false,
-        loading: true,
-        modal1: false,
-        formValidate: {
-          oldPassword: '',
-          newPassword: '',
-          resetPassword: ''
-        },
-        ruleValidate: {
-          oldPassword: [{
-            required: true,
-            message: '密码不能为空',
-            trigger: 'blur'
-          }],
-          newPassword: [{
-            required: true,
-            message: '密码不能为空',
-            trigger: 'blur'
-          }],
-          resetPassword: [{
-            required: true,
-            message: '密码不能为空',
-            trigger: 'blur'
-          }],
-        }
+        isCollapsed: false
       }
     },
     computed: {
-      iconSize() {
-        return this.spanLeft === 5 ? 14 : 24;
+      rotateIcon () {
+        return [
+          'menu-icon',
+          this.isCollapsed ? 'rotate-icon' : ''
+        ];
       },
-      logoSize() {
-        if (this.spanLeft !== 5) {
-          this.logoIsDisplay = true;
-          return 25;
-        } else {
-          this.logoIsDisplay = false;
-          return 0;
-        }
+      menuitemClasses () {
+        return [
+          'menu-item',
+          this.isCollapsed ? 'collapsed-menu' : ''
+        ]
       }
     },
     methods: {
-      toggleClick() {
-        if (this.spanLeft === 5) {
-          // this.spanLeft = 2;
-          this.spanLeft = 2;
-          // this.spanRight = 22;
-          this.spanRight = 22;
-        } else {
-          this.spanLeft = 5;
-          this.spanRight = 19;
-        }
-      },
-      modifyPassWord() {
-        this.$Message.info("您没有权限操作");
-        // this.modal1 = true;
-      },
-      logout() {
-        var loginParams = {}
-        requestLoginOut(loginParams).then(data => {
-          if (data.success == 'FAILED') {
-            this.$Message.error(data.error.message);
-          } else {
-            this.$router.push({
-              path: '/login'
-            });
-          }
-        }).catch(() => {
-
-        });
-      },
-      comfirmModifyPS() {
-        this.$refs.formValidate.validate((valid) => {
-          if (valid) {
-            this.modal1 = false;
-            //         this.loading = false;
-            this.$Message.success('提交成功!');
-          } else {
-            this.$Message.error('表单验证失败!');
-            return false;
-          }
-        })
-        // this.$Message.info('点击了确定');
-      },
-      menuSelect(name) {
-        this.$router.push({
-          path: name
-        });
-      },
-      cancel() {
-        this.modal1 = false;
-        this.$Message.info('点击了取消');
-      },
-    },
-    mounted() {
-
+      collapsedSider () {
+        this.$refs.side1.toggleCollapse();
+      }
     }
   }
 </script>
-<style scoped>
-  .layout {
-    background: #f5f7f9;
-    position: relative;
-    overflow: hidden;
-    height: 100vh;
-  }
-
-  .layout-breadcrumb {
-    padding: 10px 15px 0;
-  }
-
-  .layout-content {
-    min-height: 200px;
-    margin: 15px;
-    overflow: auto;
-    background: #fff;
-    border-radius: 4px;
-    height: 100%;
-  }
-
-  .layout-content-main {
-    padding: 10px;
-    width: 91vw;
-  }
-
-  .layout-copy {
-    text-align: center;
-    padding: 10px 0 20px;
-    color: #9ea7b4;
-  }
-
-  .layout-menu-left {
-    background: #464c5b;
-  }
-
-  .layout-header {
-    height: 60px;
-    background: #fff;
-
-  }
-
-  .layout-logo-left {
-    width: 90%;
-    height: 60px;
-    line-height: 60px;
-    font-size: 28px;
-    text-align: center;
-    /*  background: #5b6270;
-        border-radius: 3px;
-        margin: 15px auto;*/
-  }
-
-  .layout-ceiling-main a {
-    color: #9ba7b5;
-  }
-
-  .layout-hide-text .layout-text {
-    display: none;
-  }
-
-  .ivu-col {
-    transition: width .2s ease-in-out;
-  }
-
-  .ivu-row-flex {
-    height: 100%;
-  }
-
-  .userinfo {
-    display: inline-block;
-    float: right;
-  }
-
-  .userinfo .ivu-dropdown {
-    margin-top: 50px;
-  }
-
-  .ivu-dropdown {
-    margin-right: 25px;
-    margin-top: 22px;
-  }
-
-  .ivu-menu-submenu-title {
-    padding: 14px;
-  }
-
-  .head-img {
-    width: 100%;
-    height: 60px;
-    line-height: 60px;
-    float: right;
-    margin-top: -50px;
-  }
-
-  .head-img img {
-    border-radius: 20px;
-    margin: 10px 0px 10px 10px;
-    width: 40px;
-    height: 40px;
-    float: right;
-  }
-</style>
